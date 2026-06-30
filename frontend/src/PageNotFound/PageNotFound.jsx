17731e630d39b568d8bebea528743f0105520c01
eleneenukidze07 <eenuk24@freeuni.edu.ge>
2026-06-30 15:23:59 +0400

    feat: added Page not Found and mock cookie

--- frontend/src/PageNotFound/PageNotFound.jsx
+++ frontend/src/PageNotFound/PageNotFound.jsx
@@ -1,7 +1,18 @@
 import 'react'
 import {Link} from "react-router-dom";
+import {useEffect} from "react";
 
 function PageNotFound() {
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/login', {
+            method: 'Post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include'
+        })
+    }, []);
     return (
         <div>
         <h1> Page Not Found </h1>

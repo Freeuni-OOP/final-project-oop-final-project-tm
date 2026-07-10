5ee41a7ba28374ce93dbddb525d14ecff1e8261f
eleneenukidze07 <eenuk24@freeuni.edu.ge>
2026-07-10 21:12:29 +0400

    Follow logic with profiles, with comments

--- frontend/src/Profile/Services/FollowingManagement.js
+++ frontend/src/Profile/Services/FollowingManagement.js
@@ -1,7 +1,6 @@
 export const FollowingManagement = async (isFollowing, publicId)=> {
     if(isFollowing) {
 
-        console.log("dgiudgfiudfg " + isFollowing + "   ");
         const response = await fetch(`http://localhost:8080/api/profile/follow/new/${publicId}`,
             {
                 method: 'POST',
@@ -13,10 +12,8 @@ export const FollowingManagement = async (isFollowing, publicId)=> {
             throw new Error("Failed to follow");
         }
         const k = await response.json();
-        console.log("k:    " + k);
         return k;
     } else {
-        console.log("dgiudgfiudfg " + isFollowing);
         const response = await fetch(`http://localhost:8080/api/profile/follow/delete/${publicId}`,
             {
                 method: 'POST',
